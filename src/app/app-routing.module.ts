48946d2766bacc87146c37273aeddd7f10e37fd5
vijay gohel <gohil5898@gmail.com>
2021-03-09 11:18:08 +0530

    basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes=[];
+const routes: Routes=[
+    {path: 'departments', component:DepartmentListComponent},
+    {path: 'employees', component: EmployeeListComponent}
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
@@ -9,4 +14,6 @@ const routes: Routes=[];
 })
 
 export class AppRoutingModule {}
+
+export const routingComponents=[DepartmentListComponent,EmployeeListComponent];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {AppRoutingModule} from './app-routing.module';
+import {AppRoutingModule, routingComponents} from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    routingComponents
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
