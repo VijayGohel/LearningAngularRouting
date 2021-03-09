531b5c06f8c30213abf58c269b9a2f206f8dc36b
vijay gohel <gohil5898@gmail.com>
2021-03-09 09:48:12 +0530

    generating routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DepartmentListComponent } from './department-list/department-list.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DepartmentListComponent,
-    EmployeeListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
